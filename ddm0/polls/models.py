eff40580e07f9b6b7927b028c334a89be6974cd6
Nathan Nichols <broinjc@gmail.com>
2013-01-18 14:41:51 -0800

    Minor updates for fun

--- ddm0/polls/models.py
+++ ddm0/polls/models.py
@@ -12,7 +12,7 @@ class Poll(models.Model):
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
-
+    
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice = models.CharField(max_length=200)
