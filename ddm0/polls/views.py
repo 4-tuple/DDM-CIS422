4b8f6963afb3bd27a714a49ed51ae4a4e9dd516f
Nathan Nichols <broinjc@gmail.com>
2013-01-18 16:16:05 -0800

    Added views for poll details, and and index of all polls

--- ddm0/polls/views.py
+++ ddm0/polls/views.py
@@ -1,11 +1,11 @@
-from django.template import Context, loader
 from polls.models import Poll
-from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    t = loader.get_template('polls/index.html')
-    c = Context({
-        'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(t.render(c))
+    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
+
+def detail(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/detail.html', {'poll': p})
